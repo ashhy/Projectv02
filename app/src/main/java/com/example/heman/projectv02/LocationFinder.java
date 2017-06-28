b660ad2a6bdd0b83c2d8ae1d63c8747560a3cc5c
Yash Jain <techmahindraashhy@gmail.com>
2017-06-29 02:12:17 +0530

    Enabled MultiDex in Gradle

---
+++ app/src/main/java/com/example/heman/projectv02/LocationFinder.java
@@ -0,0 +1,132 @@
+package com.example.heman.projectv02;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.IntentSender;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Looper;
+import android.support.annotation.NonNull;
+
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.ResolvableApiException;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResponse;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
+import com.google.android.gms.location.SettingsClient;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+
+/**
+ * Created by yashjain on 6/29/17.
+ */
+
+final public class LocationFinder {
+
+
+    private static final long LOCATION_UPDATE_INTERVAL = 10000;
+    private static final long LOCATION_UPDATE_FASTEST = 5000;
+    private static FusedLocationProviderClient mFusedLocationProviderClient;
+    private static LocationCallback mLocationCallback;
+    private static Activity mActivity;
+    private static boolean gpsEnabled;
+    private static LocationRequest mLocationRequest;
+    private static LocationSettingsRequest mLocationSettingsRequest;
+    private static SettingsClient mSettingsClient;
+    private static final int REQUEST_CHECK_SETTINGS = 0x1;
+    private static boolean mRequestingLocationUpdates;
+
+
+    /**
+    * This function needs to be called once in the Splash Activity
+    */
+    public static void init(@NonNull Activity activity,@NonNull LocationCallback locationCallback) {
+        mActivity = activity;
+        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(mActivity);
+        mSettingsClient = LocationServices.getSettingsClient(mActivity);
+        isGpsEnabled();
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        mLocationRequest.setFastestInterval(LOCATION_UPDATE_FASTEST);
+        mLocationRequest.setInterval(LOCATION_UPDATE_INTERVAL);
+        mLocationCallback=locationCallback;
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder();
+        builder.addLocationRequest(mLocationRequest);
+        mLocationSettingsRequest = builder.build();
+    }
+
+
+    /**
+    *Call this only and only if Location Permissions have been Enabled
+    */
+    public static boolean isGpsEnabled() {
+        LocationManager locationManager = (LocationManager) mActivity.getSystemService(Context.LOCATION_SERVICE);
+        gpsEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        return gpsEnabled;
+    }
+
+
+    /**
+     * Call this whenever Location is Desired
+     * And Call this in OnResume
+     */
+    public static void startLocationUpdates() {
+        mRequestingLocationUpdates=true;
+        mSettingsClient.checkLocationSettings(mLocationSettingsRequest)
+                .addOnSuccessListener(mActivity, new OnSuccessListener<LocationSettingsResponse>() {
+                    @Override
+                    public void onSuccess(LocationSettingsResponse locationSettingsResponse) {
+                        mFusedLocationProviderClient.requestLocationUpdates(mLocationRequest,
+                                mLocationCallback, Looper.myLooper());
+                    }
+                })
+                .addOnFailureListener(mActivity, new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        int statusCode = ((ApiException) e).getStatusCode();
+                        switch (statusCode) {
+                            case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
+                                try {
+                                    // Show the dialog by calling startResolutionForResult(), and check the
+                                    // result in onActivityResult().
+                                    ResolvableApiException rae = (ResolvableApiException) e;
+                                    rae.startResolutionForResult(mActivity, REQUEST_CHECK_SETTINGS);
+                                } catch (IntentSender.SendIntentException sie) {
+                                }
+                                break;
+                            case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
+                                String errorMessage = "Location settings are inadequate, and cannot be " +
+                                        "fixed here. Fix in Settings.";
+                        }
+                    }
+                });
+    }
+
+
+
+    /**
+    *Call this in OnPause
+    *Or whenever Location is not Required
+    */
+    public static void stopLocationUpdates() {
+        if (!mRequestingLocationUpdates) {
+            return;
+        }
+        mFusedLocationProviderClient.removeLocationUpdates(mLocationCallback)
+                .addOnCompleteListener(mActivity, new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        mRequestingLocationUpdates = false;
+                    }
+                });
+    }
+
+}
--- app/src/main/java/com/example/heman/projectv02/SplashActivity.java
+++ app/src/main/java/com/example/heman/projectv02/SplashActivity.java
@@ -3,13 +3,18 @@ package com.example.heman.projectv02;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.Location;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Toast;
+
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationResult;
 
 public class SplashActivity extends AppCompatActivity {
     private static final int MY_PERMISSIONS_REQUEST_CODE = 1;
@@ -23,17 +28,36 @@ public class SplashActivity extends AppCompatActivity {
             Toast.makeText(this, "You have Permission", Toast.LENGTH_LONG).show();
         }else {
             setPermissions();
-            checkPermissions();
         }
+        if(checkPermissions()){
+            LocationFinder.init(this,new LocationCallback(){
+                @Override
+                public void onLocationResult(LocationResult locationResult) {
+                    super.onLocationResult(locationResult);
+                    Location l=locationResult.getLastLocation();
+                    Toast.makeText(SplashActivity.this,"Location Result:\nLatitude: "+String.valueOf(l.getLatitude())+"\nLongitude: "+String.valueOf(l.getLongitude()+"\nAltitude: "+String.valueOf(l.getAltitude())),Toast.LENGTH_LONG).show();
+                }
+            });
+
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        LocationFinder.startLocationUpdates();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        LocationFinder.stopLocationUpdates();
     }
 
     //Permissions
     protected boolean checkPermissions() {
-        if (ActivityCompat.checkSelfPermission(this,
-                android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            return false;
-        }
-        return true;
+        return ActivityCompat.checkSelfPermission(this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED;
     }
     protected void setPermissions() {
         ActivityCompat.requestPermissions(this, new String[]{
